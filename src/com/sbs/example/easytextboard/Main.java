9ae5b5e84112c2ea6b68b2503fe806e2193bf2e4
Dong1414 <homar1@naver.com>
2020-10-12 12:17:56 +0900

    스캐너 입력

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -1,9 +1,24 @@
 package com.sbs.example.easytextboard;
+
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("안녕");
+
+		Scanner scanner = new Scanner(System.in);	
+		System.out.print("명령어 입력: ");
+		
+		String command = scanner.nextLine();
+
+		if (command.equals("text add")) {
+			System.out.println("텍스트 추가");
+		} else if (command.equals("text list")) {
+			System.out.println("텍스트 리스트");
+		} else {
+			System.out.println("존재하지 않음");
+		}
+
 	}
 
 }
