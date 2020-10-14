e6899f2993a0280f83d14032e6472b88b00e7118
Dong1414 <homar1@naver.com>
2020-10-14 13:23:47 +0900

    클래스 구현

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -2,19 +2,16 @@ package com.sbs.example.easytextboard;
 
 import java.util.Scanner;
 
+
+
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
+		Article article1 = new Article();
+		Article article2 = new Article();
+		
 		int lastid = 0;
-
-		String[] article_title = new String[3];
-		String[] article_body = new String[3];
-		int[] article_id = new int[3];
-
-		String article2_title = "";
-		String article2_body = "";
-		int article2_id = 0;
 
 		while (true) {
 			System.out.printf("명령어 입력: ");
@@ -32,11 +29,16 @@ public class Main {
 				System.out.printf("내용: ");
 				body = scanner.nextLine();
 
-				article_title[lastid] = title;
-				article_body[lastid] = body;
-				article_id[lastid] = id;
-				
-				
+				if(id == 1) {
+				article1.title = title;
+				article1.body = body;
+				article1.id = id;
+				}
+				if(id == 2) {
+					article2.title = title;
+					article2.body = body;
+					article2.id = id;
+					}
 				System.out.printf("%d번재 게시물 등록\n", id);
 				lastid = id;
 
@@ -51,28 +53,39 @@ public class Main {
 				System.out.println("번호 / 제목");
 				if (lastid >= 1) {
 
-					System.out.printf("%d / %s\n", article_id[0], article_title[0]);
+					System.out.printf("%d / %s\n", article1.id, article1.title);
 				}
-							}
-			
+				if (lastid >= 2) {
+
+					System.out.printf("%d / %s\n", article2.id, article2.title);
+				}
+
+			}
+
 			else if (com.startsWith("article detail ")) {
 
 				int inputedid = Integer.parseInt(com.split(" ")[2]);
-				
-				if(lastid == 0){
-					System.out.printf("%d번째 게시물 존재하지 않음\n",inputedid);
+
+				if (lastid == 0) {
+					System.out.printf("%d번째 게시물 존재하지 않음\n", inputedid);
 					continue;
 				}
-				else if(lastid >= 1) {
-				
-				
-				System.out.println("== 게시물 상세 ==");
-				System.out.printf("번호: %d\n",article_id[inputedid-1]);
-				System.out.printf("제목: %s\n",article_title[inputedid-1]);
-				System.out.printf("내용: %s\n",article_body[inputedid-1]);
+				if (inputedid == 1) {
+
+					System.out.println("== 게시물 상세 ==");
+					System.out.printf("번호: %d\n", article1.id);
+					System.out.printf("제목: %s\n", article1.title);
+					System.out.printf("내용: %s\n", article1.body);
+				}
+				if (inputedid >= 2) {
+
+					System.out.println("== 게시물 상세 ==");
+					System.out.printf("번호: %d\n", article2.id);
+					System.out.printf("제목: %s\n", article2.title);
+					System.out.printf("내용: %s\n", article2.body);
 				}
 			}
-			
+
 			else if (com.equals("system exit")) {
 				System.out.println("프로그램 종료");
 				break;
