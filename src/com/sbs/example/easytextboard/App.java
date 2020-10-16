824821ff1aa37f3be16ed54429a8bb79a6e720b1
Dong1414 <homar1@naver.com>
2020-10-16 13:32:38 +0900

    if문 없애기 까지

--- src/com/sbs/example/easytextboard/App.java
+++ src/com/sbs/example/easytextboard/App.java
@@ -4,30 +4,8 @@ import java.util.Scanner;
 
 public class App {
 
-	Article article1 = new Article();
-	Article article2 = new Article();
+	Article[] article = new Article[10];
 
-
-	
-	
-	
-	public void setarticle(int id, String title, String body) {
-		
-		article1.id = id;
-		article1.title = title;
-		article1.body = body;
-	}
-	
-	
-	public Article getarticle(int id) {
-
-		if (id == 1) {
-			return article1;
-		} else if (id == 2) {
-			return article2;
-		}
-		return null;
-	}
 
 	public void run() {
 		Scanner scanner = new Scanner(System.in);
@@ -35,85 +13,60 @@ public class App {
 		int lastid = 0;
 
 		while (true) {
-			System.out.printf("명령어 입력: ");
+
+			System.out.printf("명령어: ");
+
 			String com = scanner.nextLine();
 
 			if (com.equals("article add")) {
 				System.out.println("== 게시물 등록 ==");
 
-				String title = "", body = "";
+				System.out.printf("제목 :");
+				String title = scanner.nextLine();
+
+				System.out.printf("내용 :");
+				String body = scanner.nextLine();
+
 				int id = lastid + 1;
 
-				System.out.printf("제목: ");
-				title = scanner.nextLine();
+				article[id - 1] = new Article(id, title, body);
 
-				System.out.printf("내용: ");
-				body = scanner.nextLine();
-				
-				Article article = new Article();
-				
-				
-				if (id == 1) {
-					article1.title = title;
-					article1.body = body;
-					article1.id = id;
-				}
-				if (id == 2) {
-					article2.title = title;
-					article2.body = body;
-					article2.id = id;
-				}
-				System.out.printf("%d번재 게시물 등록\n", id);
-				lastid = id;
-
-			}
-
-			else if (com.equals("article list")) {
+				System.out.printf("%d번째 게시물이 등록되었습니다.\n", id);
+				lastid++;
+			} else if (com.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
-				
-				
-				
+
 				if (lastid == 0) {
-					System.out.println("== 게시물 없음 ==");
+					System.out.println("== 게시물이 존재하지 않습니다. ==");
+				}
+
+				for (int i = 0; i < lastid; i++) {
+
+					System.out.printf("번호 : %d\n", article[i].id);
+					System.out.printf("제목 : %s\n", article[i].title);
+					System.out.printf("내용 : %s\n", article[i].body);
+					System.out.printf("=============\n");
+				}
+
+			} else if (com.startsWith("article detail ")) {
+				int inputid = Integer.parseInt(com.split(" ")[2]);
+
+				if (lastid < inputid) {
+					System.out.printf("%d번글이 존재하지 않습니다.\n", inputid);
 					continue;
+				} else {
+
+					System.out.printf("번호: %d\n", article[inputid - 1].id);
+					System.out.printf("제목: %s\n", article[inputid - 1].title);
+					System.out.printf("내용: %s\n", article[inputid - 1].body);
 				}
-				System.out.println("번호 / 제목");
-				if (lastid >= 1) {
-
-					System.out.printf("%d / %s\n", article1.id, article1.title);
-				}
-				if (lastid >= 2) {
-
-					System.out.printf("%d / %s\n", article2.id, article2.title);
-				}
-
-			}
-
-			else if (com.startsWith("article detail ")) {
-
-				int inputedid = Integer.parseInt(com.split(" ")[2]);
-
-				Article selectedArticle = getarticle(inputedid);
-
-				if (selectedArticle == null || selectedArticle.id == 0) {
-					System.out.printf("%d번째 게시물 존재하지 않음\n", inputedid);
-					continue;
-				}
-				System.out.println("== 게시물 상세 ==");
-				System.out.printf("번호: %d\n", selectedArticle.id);
-				System.out.printf("제목: %s\n", selectedArticle.title);
-				System.out.printf("내용: %s\n", selectedArticle.body);
-
-			}
-
-			else if (com.equals("system exit")) {
+			} else if (com.equals("system exit")) {
 				System.out.println("프로그램 종료");
 				break;
 			} else
 				System.out.println("명령어 없음");
 
 		}
-		scanner.close();
 
 	}
 
--- src/com/sbs/example/easytextboard/Article.java
+++ src/com/sbs/example/easytextboard/Article.java
@@ -1,8 +1,14 @@
 package com.sbs.example.easytextboard;
 
 public class Article {
+	
 	public int id;
 	public String title;
 	public String body;
-	
+
+	public Article(int setid, String settitle, String setbody) {
+		id = setid;
+		title = settitle;
+		body = setbody;
+	}
 }
